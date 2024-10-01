1746ad4e502dd0110a0192167f3eb812bd40ebad
kateryna_kornysheva <k.kornysheva@betinvest.com>
2024-10-01 21:24:10 +0200

    done

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,3 +1,4 @@
+import "../css/common.css";
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,3 +1,5 @@
+import "../css/common.css";
+
 let formData = {
   email: "",
   message: ""
